acc59196199d03cdc879a2360c0eb6cbe382edc4
Monika Cyprys <monko.projects@gmail.com>
2021-04-05 16:54:06 +0200

    finish exercise 02 - loops

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,27 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
 
-
+// let x = prompt("Podaj liczbę!");
+// let multi;
+// while (x > 9) {
+//   x = prompt("Podaj liczbę mniejszą niż 9!")
+// }
+// for (let i = 1; i <= 9; i++) {
+// multi = i * x;
+// console.log(multi);
+// }
 
 /* rozwiązanie z pętlą while  */
+
+const a = prompt("podaj podstawę liczby!");
+const n = prompt("podaj wykładnik liczby!");
+let iteration = 0;
+let result = a;
+let equation = a;
+
+while (iteration < n - 1) {
+  iteration++;
+  result = result * a;
+  equation += ` * ${a}`;
+}
+console.log(`${equation} = ${result}`);
+console.log(a, n);
