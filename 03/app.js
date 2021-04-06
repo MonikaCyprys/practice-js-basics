9d99c3122b26492d634a29fecd5a07d457f9e204
Monika Cyprys <monko.projects@gmail.com>
2021-04-06 18:50:01 +0200

    finish exercise 03 - functions

--- 03/app.js
+++ 03/app.js
@@ -5,14 +5,45 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-console.log(a, b, c);
+const tabOfNumbers = [a, b, c];
 
+function compareNumbers(a, b) {
+  return a - b;
+}
 
+function getSum(a, b, c) {
+  tabOfNumbers.sort(compareNumbers);
+  const twoTheBiggestNum = tabOfNumbers.slice(-2);
+  return twoTheBiggestNum[0] + twoTheBiggestNum[1];
+}
 
+const isEven = function (sum) {
+  if (typeof sum === "number") {
+    const checkSum = sum % 2 === 0 ? true : false;
+    return checkSum;
+  } else return null;
+};
 
+const sum = getSum();
+const even = isEven(sum);
 
+const showInfo = function (sum, even) {
+  console.log(sum, even);
+  switch (even) {
+    case null:
+      console.log(`Podany argument ${sum} nie jest liczbą`);
+      break;
+    case true:
+      console.log(`Podany argument ${sum} jest parzysty`);
+      break;
+    case false:
+      console.log(`Podany argument ${sum} jest nieparzysty`);
+      break;
+  }
+};
 
+showInfo(sum, even);
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
